7855011e8c7a2f4d8ccde5ba71cf47cfbf7bcc21
wards-a <mawardiadama@gmail.com>
2026-09-07 23:28:23 +0700

    update CORS

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://kelana-ai-rho.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
